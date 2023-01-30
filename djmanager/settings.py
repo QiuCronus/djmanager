3134a6f1c4ba89e9d7872c29dae390c69c2afe8f
QiuCronus <437301436@qq.com>
2023-01-30 17:34:19 +0800

    setting STATIC_ROOT

--- djmanager/settings.py
+++ djmanager/settings.py
@@ -20,6 +20,8 @@ dotenv.load_dotenv("settings.env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = os.path.join(os.getcwd(), "static")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
